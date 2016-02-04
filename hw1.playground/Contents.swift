495025b5a75204f25806e4c8cb435df89a960d1c
Clark Chen <atclarkchen@gmail.com>
2016-02-03 20:52:53 -0800

    hw1 first commit

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -22,49 +22,50 @@ class Words {
 //: ### variables the same type? If not, why?
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: Different Types; the instance variables are of type String, and the values passed in to the init function are String optional.
 
 
 //: ## Q2: Variable Types and Function Types
-    func arePalindromes(words: [String]) -> Bool {
+    class func arePalindromes(words: [String]) -> Bool {
         let reversedWords = words.map() {String($0.characters.reverse())}
-        var numElements = words.count
+        let numElements = words.count
         
-        for let i = 0; i < numElements; i++ {
+        for var i = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
+        return true
     }
 //: ### Why does the compiler dislike the **for loop**? Fix it.
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
-
+//: 1) Compiler dislikes the CONSTANT "i" in the for loop loop; "i" is an immutable object, so we can't use it as a counter. Change "let" to "var. 2) numElements could be renamed as Constant (use Let instead of Var) since it is never changed. In addition, func arePalindromes is an INSTANCE FUNCTION. CODE AT BOTTOM calls Words.arePalindromes (uses it as CLASS FUNCTION).  Therefore, we must change arePalindromes from an instance function to class function by adding "class" to method signature.  In addition, it was orignally possible for the function to NOT RETURN ANYTHING (did not have return true logic).  The method must return a boolean value.  Fix this by adding "return true".
 
 //: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
-        var countLetters : [Character : Int]
-        var lenA = self.wordA.characters.count
-        var lenB = self.wordB.characters.count
+    func isAnagram() -> Bool {
+        var countLetters : [Character : Int] = [Character : Int]()
+        let lenA = self.wordA.characters.count //instance member wordA cannot be used on type 'Words'
+        let lenB = self.wordB.characters.count
         
         if lenA != lenB {
             return false
         }
         
-        var arrA = Array(self.wordA.characters)
+        var arrA = Array(self.wordA.characters) //char Array of wordA
         var arrB = Array(self.wordB.characters)
         
-        for i in 0...lenA-1 {
-            let letter = arrA[i]
-            if let val = countLetters[letter] {
-                countLetters[letter] = val + 1
+        for i in 0...lenA-1 { // "i" is the index of each element in wordA
+            let letter = arrA[i] // letter = element (character) of wordA
+            if let val = countLetters[letter] { // val = countLetters[letter]-->gives you COUNT(int) of the character in dictionary unwrapped
+                //if there is a value (count) corresponding to the (char) "letter" in the dictionary, then...
+                countLetters[letter] = val + 1 // increment that count by 1 in the dictionary
             } else {
-                countLetters[letter] = 1
+                countLetters[letter] = 1 //else: set count to 1
             }
-        }
+        } //count the number of chars in WordA
         
         for i in 0...lenB-1 {
             let letter = arrB[i]
@@ -81,16 +82,14 @@ class Words {
             }
         }
         
-        return nil
+        return true
     }
 //: ### What is the problem with declaring **countLetters** as we do in **Line X**,
 //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
-
-//: [EXPLAIN YOUR ANSWER HERE]
-    
+//:Error: countLetters was DECLARED in Line X, but NOT INITIALIZED (it had a null value). Thus, at Line Y ("if let val = countLetters[letter]") it was USED BEFORE BEING INITIALIZED. Fix this by INITIALIZING countLetters dictionary in Line X. isAnagram() is a CLASS FUNC as declared originally. However, CODE BELOW calls isAnagram() on an INSTANCE of class words (uses it as instance function). Fix this by changing isAnagram() to instance function by getting rid of CLASS.  isAnagram() is also declared to be a boolean function, but last line had a "return nil"; should be changed to returning a boolean (possibly true), since true is not returned anywhere previously.  CHANGE TO "RETURN TRUE".
     
 }
 
